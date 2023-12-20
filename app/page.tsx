e264b3cc2e43620900de97f8942958db8b67319d
Rathna Chinthalapalli <hellorathnac@gmail.com>
2023-12-19 17:23:51 -0800

    chore: reorder imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
-import { lusitana } from './ui/fonts';
 import Image from 'next/image';
+
+import styles from '@/app/ui/home.module.css';
+import AcmeLogo from '@/app/ui/acme-logo';
+import { lusitana } from './ui/fonts';
 
 export default function Page() {
   return (
